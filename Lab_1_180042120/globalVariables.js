6ccc265fe7790b805c566dec7d650b26ec121043
sakib20 <sakib20@iut-dhaka.edu>
2021-05-07 07:14:42 +0600

    add globalVariables.js

---
+++ Lab_1_180042120/globalVariables.js
@@ -0,0 +1,14 @@
+let currentDirectory = __dirname
+console.log(currentDirectory)
+
+let fileName = __filename
+console.log(fileName)
+console.log(require('fs'))
+
+//module - different files (for code readability)
+
+console.log(module)
+
+//process - info about where the prog. is executed
+console.log(process)
+
